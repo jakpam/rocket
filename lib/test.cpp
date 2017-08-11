f23bdce00d6093a06a375b660c5f10cc93db6ad0
Jakub Pamulski <jakub.pamulski@gmail.com>
2017-08-11 07:16:25 +0200

    dos test updated

--- lib/test.cpp
+++ lib/test.cpp
@@ -1,24 +1,28 @@
+#include <conio.h>
 #include <stdio.h>
 #include <stdarg.h>
 #include <math.h>
 
-#include <unistd.h>
-#include <ncurses.h>
-
 #include "sync.h"
 
-static double _row = 0.0;
-static double _row_add = 1.0;
+#if 0
+#define TIME_INC (1.0/60.0)
+#else
+#define TIME_INC (1.0)
+#endif
+
+static const float bpm = 60.0f; /* beats per minute */
+static const int rpb = 1; /* rows per beat */
+static const double row_rate = (double(bpm) / 60) * rpb;
+
+static double time = 0.0;
+static double time_inc = TIME_INC;
 
 static double bass_get_row(void *)
 {
-    double ret_val = _row;
-
-    printf("bass_get_row: %d\n", ret_val);
-
-    _row += _row_add;
-
-    return ret_val;
+    double ret_val = time * row_rate;
+    printf("bass_get_row: %f\n", ret_val);
+	return ret_val;
 }
 
 #ifndef SYNC_PLAYER
@@ -27,25 +31,26 @@ static void bass_pause(void *v, int i)
 {
     if (i)
     {
-        _row_add = 0.0;
+        time_inc = 0.0;
     }
     else
     {
-        _row_add = 1.0;
+        time_inc = TIME_INC;
     }
     printf("bass_pause: %d\n", i);
 }
 
-static void bass_set_row(void *v, int i)
+static void bass_set_row(void *v, int row)
 {
-    _row = i;
-    printf("bass_set_row %f\n", _row);
+    time = row / row_rate;
+    printf("bass_set_row time: %f, row: %d\n", time, row);
 }
 
 static int bass_is_playing(void *v)
 {
-    printf("bass_is_playing %f\n", _row_add);
-    return _row_add;
+    int ret_val = TIME_INC == time_inc;
+    printf("bass_is_playing %d\n", ret_val);
+    return ret_val;
 }
 
 static struct sync_cb bass_cb = {
@@ -74,12 +79,14 @@ int main(int argc, char *argv[])
     const sync_track *b_a = sync_get_track(rocket, "b:a");
     const sync_track *b_b = sync_get_track(rocket, "b:b");
 
-    while (ERR == getch())
+    while (!kbhit())
     {
         double row = bass_get_row(NULL);
 
+        if (row >= 0x80) break;
+
 #ifndef SYNC_PLAYER
-        if (sync_update(rocket, (int)floor(row), &bass_cb, NULL))
+        if (sync_update(rocket, /*(int)floor(row)*/row, &bass_cb, NULL))
             sync_tcp_connect(rocket, "localhost", SYNC_DEFAULT_PORT);
 #endif
 
@@ -91,6 +98,12 @@ int main(int argc, char *argv[])
         printf("b:a:%f b:b:%f\n",
                 sync_get_val(b_a, row),
                 sync_get_val(b_b, row));
+
+#if 0
+        sleep(1);
+#endif
+
+        time += time_inc;
     }
 
 #ifndef SYNC_PLAYER
